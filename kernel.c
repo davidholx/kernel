5476d8360f6703b1b8b6ff1035176979a32116bc
Pawel Pawel <pawel@pawel.com>
2020-11-01 17:51:01 +0100

    Niewielkie modyfikacje

--- kernel.c
+++ kernel.c
@@ -5,5 +5,6 @@ int main() {
   printf("That is the kernel\n");
   printf("And that is a modification\n");
   printf("Modifications by another user\n");
+  printf("Yet another modification by a user\n");
   return 0;
 }
